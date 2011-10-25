6837398d358e3a980a4de5b1d5fdd9ac3ad56aa4
Ross Morsali <ross@designsandcode.com>
2011-10-25 12:17:13 +0100

    Various Updates including multi line support, minified version, updated examples.
    Refactored the JS so it now works more efficiently and should be easier to use.

--- autoellipsis.jquery.js
+++ autoellipsis.jquery.js
@@ -1,8 +1,8 @@
 ﻿/*
 	Name: Auto Ellipsis Text with jQuery
-	Date: August 2011
+	Date: October 2011
 	Description: Auto Ellipsis Text with jQuery, based on Brandon Cannaday's Auto Ellipse script at: http://www.switchonthecode.com/tutorials/javascript-tutorial-how-to-auto-ellipse-text
-	Version: 1.0
+	Version: 1.1
 	Author: Ross Morsali
 	Author URI: http://www.weareelevate.com
 	Project URI: https://github.com/rmorse/AutoEllipsis
@@ -13,47 +13,95 @@ jQuery.fn.autoEllipsisText = function(options)
 	//WIDTH is currently used to manually define a width for the content area and to begin clipping the text - if none is provided the width of the DOM element will be used by default
 	//OFFSET would normally be used when WIDTH above is not used, this can be used to increase or decrease the clipping area from the parents element. A negative value will have a padding effect clipping text before the edges of the DOM element
 	
-	if(!options)
-	{
-		var options = new Array();
-	}
-	//initalise variables
-	var width = 0;
-	var text = "";
-	var offset = 0;
+	//initalise defaults
+	var defaults = {
 	
-	var twidth = "";
-	if(options["width"]){twidth = options["width"];}
-	if(twidth!=""){width = options["width"];}
+		width: 0, //manually set the width the text needs to fit in - overrides the width of the containing element
+		height: 0, //the manually set a height the text needs to fit in - only works if multiline is "true" - overrides the height of the containing element.
+		offset: 0, //to manually add/remove a certain amount of width in px
+		persist: 0, //add event listener to the div to check on resize, if resize occures the script will recalculate (not advised)
+		addTitle: true, //this allows the full text to be added to the title attribute of the span tag so users will see full text on mouse over - applies to single line only
+		multiline: false, // default for single line processing - could be a processor killer if used on a lot of body text throughout a page
+		accuracyStep: 1 //increasing the variable will increase performance however it may be less accurate - only applies to multiline	
+		
+	};
 	
-	var toffset = "";
-	if(options["offset"]){toffset = options["offset"];}
-	if(toffset!=""){offset = toffset;}
+	var text = '';
 	
-	//loop through each occurence
+	//initialise options
+	var opts = jQuery.extend(defaults, options);
+	
+	//loop through each item matched
 	this.each(function()
 	{
-		if(width==0)
-		{
-			width = $(this).width()+offset;
+		var item = $(this);
+		var itemHeight = opts.height;
+		var itemWidth = opts.width;
+		
+		if((opts.width==0))
+		{//if no width has been manually set grab the width, if offset has been set it will be automatically added
+			itemWidth = item.width()+opts.offset;
+			
+		}
+		if(opts.height==0)
+		{//if height has not been set then grab the current height of the div
+			itemHeight = item.height();
 		}
 		
-		text = $(this).html();
-		$(this).html('<span title="' + text + '" class="ellipsisSpan" style="white-space:nowrap;">' + text + '</span>');
-		inSpan = $(this).find('span');
+		//collect text currently residing in matched elements
+		text = item.html();
 		
-		if(inSpan.width() > width)
-		{
-			var i = 1;
-			inSpan.html("");
-			while(inSpan.width() < (width) && i < text.length)
+		if(opts.multiline==true)
+		{//if it is a multiline then add span tags
+			item.html('<span>' + text + '</span>');
+		}
+		else
+		{//if it is a single line for content to be on one line using white-space:nowrap
+			if(opts.addTitle)
 			{
-				inSpan.html(text.substr(0,i) + '...');
-				i++;
+				item.html('<span title="' + text + '" style="white-space:nowrap;">' + text + '</span>');
 			}
-			returnText = inSpan.html();
-			
-			
+			else
+			{
+				item.html('<span style="white-space:nowrap;">' + text + '</span>');
+			}
+		}
+		inSpan = item.find('span');
+		
+		if(opts.multiline)
+		{
+			var spanHeight = inSpan.height();
+			if(spanHeight > itemHeight)
+			{
+				
+				var i = text.length;
+				inSpan.html("");
+
+				while ((spanHeight > (itemHeight))&&(i>0))
+				{
+					inSpan.html(text.substr(0,i) + '...');
+					spanHeight = inSpan.height();
+					i = i-opts.accuracyStep;
+				}
+				returnText = inSpan.html();
+			}
+		}
+		else
+		{
+			var spanWidth = inSpan.width();
+			if(spanWidth > itemWidth)
+			{
+				var i = 1;
+				inSpan.html("");
+				spanWidth = 0;
+				while(inSpan.width() < (itemWidth) && i < text.length)
+				{
+					inSpan.html(text.substr(0,i) + '...');
+					spanWidth = inSpan.width();
+					i++;
+				}
+				returnText = inSpan.html();
+			}
 		}
 	});
 	return this;
